8372b932aed4f44b202adab390c0b7ee69c5fbcf
villalon <villalon@gmail.com>
2016-04-20 11:03:13 -0300

    Basic version

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ * @package local
+ * @subpackage tics331
+ * @copyright 2012-onwards Jorge Villalon <jorge.villalon@uai.cl>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
+
+// Parameter passed from the url.
+$name = required_param('name', PARAM_TEXT);
+
+$context = context_system::instance();
+$PAGE->set_context($context);
+
+// Check that user is logued in the course.
+require_login();
+
+// Page navigation and URL settings.
+$PAGE->set_url(new moodle_url('local/tics331', array('filter'=>$name)));
+$PAGE->set_pagelayout('incourse');
+$PAGE->set_title('Hello world');
+
+echo $OUTPUT->header();
+
+echo $OUTPUT->footer();
